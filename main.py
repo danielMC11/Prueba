411cdcd50407794b3c3ff276ccd141fd7144734e
Daniel MC <danielcarrillo3200@gmail.com>
2024-11-18 20:48:51 -0500

    correcion

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def sumar(a, b):
 	return a + b
@@ -14,10 +5,10 @@ def sumar(a, b):
 def restar(a, b):
 	return a - b
 
-def multiplciar(a ,b):
+def multiplicar(a ,b):
 	return a * b
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('HOLA MUNDO')
-#Comentario
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+	n1 = 5
+	n2 = 2
+	#Implementar
